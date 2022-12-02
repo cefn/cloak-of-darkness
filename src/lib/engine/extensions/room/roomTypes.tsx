f519b2c2deb7c7952b4e9c778060c22ef7b99926
Cefn Hoile <github.com@cefn.com>
2022-12-02 17:17:03 +0000

    Change and comment room type

--- src/lib/engine/extensions/room/roomTypes.tsx
+++ src/lib/engine/extensions/room/roomTypes.tsx
@@ -1,24 +1,7 @@
 import { ActionSequence, Passage } from "../../types";
 import { END } from "./roomStory";
 
-/** World state having at least a current room and titles for each room. Stories
- * will extend this */
-export type RoomWorldState<RoomId extends string> = {
-  currentRoomId: RoomId;
-  roomTitles: { [id in RoomId]: Passage };
-};
-
-/** Rooms are ActionSequences named by ids, whose sequences result in the next
- * RoomId. This structure defines a lookup for room ActionSequences that
- * terminate with either a destination RoomId or END. */
-export type RoomLookup<
-  RoomId extends string,
-  WorldState extends RoomWorldState<RoomId>
-> = {
-  [id in RoomId]: (state: WorldState) => ActionSequence<RoomId | typeof END>;
-};
-
-/** An options object with both WorldState definition and RoomId lookup - enough
+/** An options object to combine WorldState definition and RoomId lookup - enough
  * to execute a roomStory */
 export type RoomStoryOptions<
   RoomId extends string,
@@ -26,4 +9,20 @@ export type RoomStoryOptions<
 > = {
   worldState: WorldState;
   rooms: RoomLookup<RoomId, WorldState>;
+};
+
+/** World state having at least a current room and titles for each room. Room
+ * based stories should extend this */
+export type RoomWorldState<RoomId extends string> = {
+  currentRoomId: RoomId;
+  roomTitles: { [id in RoomId]: Passage };
+};
+
+/** A lookup for rooms -  ActionSequences named by ids, whose sequences result
+ * in the next RoomId or END. */
+export type RoomLookup<
+  RoomId extends string,
+  WorldState extends RoomWorldState<RoomId>
+> = {
+  [id in RoomId]: (state: WorldState) => ActionSequence<RoomId | typeof END>;
 };
